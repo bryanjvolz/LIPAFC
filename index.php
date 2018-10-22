3fec1d07175baf11e2f7c271e2ca1cdbb81813de
Bryan Volz <bryan@typocracy.net>
2018-10-22 01:28:18 -0400

    Add stylesheets, new fonts, swap index for PHP version

---
+++ index.php
@@ -0,0 +1,213 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title><?php echo date('Y'); ?> Louisville-Indianapolis Proximity Association Football Contest | LIPAFC.com</title>
+        <meta name="description" content="Louisville-Indianapolis Proximity Association Football Contest ">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/normalize.css">
+        <link rel="stylesheet" href="assets/css/main.css">
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <header>
+          <nav class="main-nav">
+            <ul>
+              <li class=""><a href="#intro" aria-label="LIPAFC.com Home"><img src="assets/img/logos/lipafc_cup.svg" alt="LIPAFC Cup Icon"></a></li>
+              <li class=""><a href="#latest-results">Latest Results</a></li>
+              <li class=""><a href="#about-lipafc">LIPAFC Info</a></li>
+              <li class=""><a href="#results">Results</a></li>
+              <li class=""><a href="#featured-matchups">Looking Back</a></li>
+            </ul>
+          </nav>
+          <section class="intro wrap">
+              <div class="container">
+                  <h1><span>The</span> Louisville-Indianapolis Proximity Association Football Contest</h1>
+                  <p><span class="hidden">'Lee-pah-eff-see' -</span> THE SINGLE MOST IMPORTANT SPORTING AND CULTURAL EVENT IN HUMAN HISTORY</p>
+              </div>
+            </section>
+        </header>
+
+        <main>
+
+          <section id="latest-results" class="latest-results">
+            <span>Latest Results: October 20<sup>th</sup> 2018</span>
+            <div class="">
+              <span class="">4</span>
+              <img src="assets/img/logos/louisville_city.svg" alt="Louisville City Logo">
+              <span class="">LOU</span>
+            </div>
+            <div class="">
+              <span class="">1</span>
+              <img src="assets/img/logos/indy_eleven_logo.svg" alt="Indianapolis 11 Logo">
+              <span class="">INDY</span>
+            </div>
+          </section>
+
+          <section class="wrap about-lipafc">
+            <div class="container">
+              <h1>The Heck is this?</h1>
+              <p>The LIPAFC (Louisville-INDIANAPOLIS PROXIMITY&hellip; oh don't make me type it again) is the soccer rivalry between Indianapolis Eleven and Louisville City FC. As aluded above, this clash of titans is the cause for humanity’s ascension to control this planet. Much like Bill &amp; Ted, this rivalry has spanned the ages and will some day bring peace to the world&hellip; once they learn to play guitar. While sporting celebs will tell you grand tales of Worldly Cups or Superb Owls, only the LIPAFC will tug your heartstrings like a puppy and tear you apart like finding out what happened to Bambi’s mom.</p>
+
+              <h3>PREPARE YOURSELF EARTHLINGS, FOR THE LIPAFC</h3>
+            </div>
+          </section>
+
+          <section class="results">
+            <div class="container">
+            <h1>LIPAFC Match History</h1>
+            <nav>
+              <select id="year-select" class="year-select">
+                <optgroup>
+                  <option value="2018">2018</option>
+                  <option value="2017">2017</option>
+                  <option value="2016">2016</option>
+                  <option value="2015">2015</option>
+                </optgroup>
+                <optgroup>
+                  <option value="1921">1921</option>
+                  <option value="1925">1925</option>
+                  <option value="1932">1932</option>
+                  <option value="1936">1936</option>
+                  <option value="1946">1946</option>
+                </optgroup>
+              </select>
+            </nav>
+              <table>
+                  <thead>
+                    <tr>
+                      <th>Date</th>
+                      <th>Score</th>
+                      <th>Winner</th>
+                      <th>Location</th>
+                      <th>Competition</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr data-year="2018">
+                      <td>October 20, 2018</td>
+                      <td>4-1</td>
+                      <td>Louisville</td>
+                      <td>Slugger Field</td>
+                      <td>USL Playoffs</td>
+                    </tr>
+                    <tr data-year="2018">
+                      <td>October 13, 2018</td>
+                      <td>0 - 1 (PK)</td>
+                      <td>Louisville</td>
+                      <td>Slugger Field</td>
+                      <td>USL</td>
+                    </tr>
+                    <tr data-year="2018">
+                      <td>August 5, 2018</td>
+                      <td>2 - 2</td>
+                      <td>-DRAW-</td>
+                      <td>Slugger Field</td>
+                      <td>USL</td>
+                    </tr>
+                    <tr data-year="2018">
+                      <td>May 5, 2018</td>
+                      <td>1 (PK) - 0</td>
+                      <td>Indianapolis</td>
+                      <td>Lucas Oil Stadium</td>
+                      <td>USL</td>
+                    </tr>
+                    <tr data-year="2017">
+                      <td>March 18, 2017</td>
+                      <td>1 - 1</td>
+                      <td>-DRAW-</td>
+                      <td>Evansville, IN</td>
+                      <td>Friendly</td>
+                    </tr>
+                    <tr data-year="2016">
+                      <td>June 1, 2016</td>
+                      <td>2 - 1</td>
+                      <td>Indianapolis</td>
+                      <td>Carroll Stadium, IUPUI</td>
+                      <td>US Open Cup</td>
+                    </tr>
+                    <tr data-year="2016">
+                      <td>March 19, 2016</td>
+                      <td>1 - 0</td>
+                      <td>Louisville</td>
+                      <td>Slugger Field <sup title="Closed to public">*</sup></td>
+                      <td>Friendly</td>
+                    </tr>
+                    <tr data-year="2015">
+                      <td>May 27, 2015</td>
+                      <td>0 - 2</td>
+                      <td>Louisville</td>
+                      <td>Carroll Stadium, IUPUI</td>
+                      <td>US Open Cup</td>
+                    </tr>
+
+                    <tr data-year="ancient">
+                      <td>1280 BC - 2015 AD</td>
+                      <td>???</td>
+                      <td>Lost to history</td>
+                      <td>The old country, the new world</td>
+                      <td>Only the Watchers know</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+            </div>
+          </section>
+
+          <section class="featured-matchups">
+            <div class="container">
+              <ul class="">
+                <li class="" data-year="2018">
+                  <h2>2018 Playoffs</h2>
+                  <p><em>Someone</em> hired a plane to fly over Slugger Field in the run up to the first meeting of the two teams
+                </li>
+                <li class="" data-year="">
+                </li>
+                <li class="" data-year="">
+                </li>
+                <li class="" data-year="">
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          <section class="photos">
+            <div class="container">
+            </div>
+          </section>
+        </main>
+
+        <footer>
+          <div class="container">
+            <ul class='team-links'>
+              <li><a href="//louisvillecityfc.com/" target="_blank">Louisville City FC</a></li>
+              <li><a href="//www.indyeleven.com/" target="_blank">Indianapolis Eleven</a></li>
+            </ul>
+
+            <ul class='supporter-links'>
+              <li><a href="//www.louisvillecoopers.com/" target="_blank">Louisville Coopers</a></li>
+              <li><a href="//www.brickyardbattalion.com/" target="_blank">Brickyard Battalion</a></li>
+              <li><a href="//scouseshouse.com/" target="_blank">Scouse's House</a></li>
+              <li><a href="//slaughterhousenineteen.com/" target="_blank">Slaughterhouse 19</a></li>
+            </ul>
+          </div>
+        </footer>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127480715-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-127480715-1');
+        </script>
+
+    </body>
+</html>
