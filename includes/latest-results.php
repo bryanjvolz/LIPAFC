831e5da9c14d2518e1d5912e3a5283967528bd3c
Bryan Volz <bryan@typocracy.net>
2022-03-28 10:29:44 -0400

    Latest 2022 results

--- includes/latest-results.php
+++ includes/latest-results.php
@@ -1,5 +1,5 @@
 <section id="latest-results" class="latest-results">
-  <span>Latest Results: <!--<a href="https://www.uslchampionship.com/indyeleven-louisvillecityfc-2034113" target="_blank" rel="noopener noreferrer">-->Oct. 16<sup>th</sup> 2021<!--</a>--></span>
+  <span>Latest Results: <!--<a href="https://www.uslchampionship.com/indyeleven-louisvillecityfc-2034113" target="_blank" rel="noopener noreferrer">-->March 26<sup>th</sup> 2022<!--</a>--></span>
   <div class="latest-results__result">
     <span class="latest-results__score">1</span>
     <div class="latest-results__team">
@@ -8,7 +8,7 @@
     </div>
   </div>
   <div class="latest-results__result">
-    <span class="latest-results__score">0</span>
+    <span class="latest-results__score">1</span>
     <div class="latest-results__team">
       <img src="assets/img/logos/louisvillecity_2020_logo.png" alt="Louisville City Logo" class="latest-results__logo">
       <span>LOU</span>
@@ -16,6 +16,6 @@
   </div>
 
 
-  <span>Next Match: <a href="https://www.loucity.com/tickets" target="_blank" rel="noopener noreferrer">March 26<sup>th</sup>, 2022</a></span>
+  <span>Next Match: <a href="https://www.indyeleven.com/tickets/" target="_blank" rel="noopener noreferrer">August 31<sup>st</sup>, 2022</a></span>
 
 </section>
