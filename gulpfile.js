22e761aa30d7a0ac92b1f762c1ed13dc8c2254b3
Bryan Volz <bryan@typocracy.net>
2022-09-02 15:30:59 -0400

    Update packages, re-run gulp and fix compatibility issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer  = require('gulp-autoprefixer');
-var browserSync   = require('browser-sync');
-var sourcemaps = require('gulp-sourcemaps');
-var uglifycss = require('gulp-uglifycss');
-var uglify = require('gulp-uglify');
-var pump = require('pump');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer  = require('gulp-autoprefixer');
+const browserSync   = require('browser-sync');
+const sourcemaps = require('gulp-sourcemaps');
+const uglifycss = require('gulp-uglifycss');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
 
 gulp.task('default',function() {
   gulp.watch('assets/sass/*.scss',gulp.series('sass'));
@@ -17,7 +17,7 @@ gulp.task('default',function() {
 gulp.task('sass', function () {
  gulp.src('assets/sass/*.scss')
   .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
   .pipe(uglifycss({
     "maxLineLen": 80,
     "uglyComments": true
