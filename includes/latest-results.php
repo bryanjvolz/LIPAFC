6a99536bb1807e9061a7aa1fefbe2e0c39ffd2af
Bryan Volz <bryan@typocracy.net>
2023-11-17 11:17:21 -0500

    Update match info, add latest highlight youtube links

--- includes/latest-results.php
+++ includes/latest-results.php
@@ -1,5 +1,5 @@
 <section id="latest-results" class="latest-results">
-  <span>Latest Results: <a href="https://www.youtube.com/watch?v=WylvaApfy60" target="_blank" rel="noopener noreferrer">May 27<sup>th</sup> 2022</a></span>
+  <span>Latest Results: <a href="https://www.youtube.com/watch?v=3nEnZNj6jrM" target="_blank" rel="noopener noreferrer">July 29<sup>th</sup> 2023</a></span>
   <div class="latest-results__result">
     <span class="latest-results__score">0</span>
     <div class="latest-results__team">
@@ -8,13 +8,13 @@
     </div>
   </div>
   <div class="latest-results__result">
-    <span class="latest-results__score">1</span>
+    <span class="latest-results__score">2</span>
     <div class="latest-results__team">
       <img src="assets/img/logos/louisvillecity_2020_logo.webp" alt="Louisville City Logo" class="latest-results__logo" width="75px" height="100px">
       <span>LOU</span>
     </div>
   </div>
 
-  <span>Next Match: <a href="https://seatgeek.com/indy-eleven-at-louisville-city-fc-tickets/united-soccer-league/2023-07-29-8-pm/5910394" target="_blank" rel="noopener noreferrer">July 29th<sup>st</sup>, 2022</a></span>
+  <span>Next Match: TBA in 2024</span>
 
 </section>
