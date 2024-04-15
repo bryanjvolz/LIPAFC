6cb5dc2192581502eafcda46fa392e1ff9d9cd62
Bryan V <bryan@typocracy.net>
2024-04-14 23:04:41 -0400

    Update results from April 2024 (#37)
    Update results from April 2024 (#37)

--- includes/latest-results.php
+++ includes/latest-results.php
@@ -1,20 +1,20 @@
 <section id="latest-results" class="latest-results">
-  <span>Latest Results: <a href="https://www.youtube.com/watch?v=3nEnZNj6jrM" target="_blank" rel="noopener noreferrer">July 29<sup>th</sup> 2023</a></span>
+  <span>Latest Results: <a href="https://www.youtube.com/watch?v=cHm-cqtCBcw" target="_blank" rel="noopener noreferrer">April 6<sup>th</sup> 2023</a></span>
   <div class="latest-results__result">
-    <span class="latest-results__score">0</span>
+    <span class="latest-results__score">3</span>
     <div class="latest-results__team">
       <img src="assets/img/logos/indy_eleven_logo.svg" alt="Indianapolis 11 Logo" class="latest-results__logo" width="75px" height="104px">
       <span>INDY</span>
     </div>
   </div>
   <div class="latest-results__result">
-    <span class="latest-results__score">2</span>
+    <span class="latest-results__score">5</span>
     <div class="latest-results__team">
       <img src="assets/img/logos/louisvillecity_2020_logo.webp" alt="Louisville City Logo" class="latest-results__logo" width="75px" height="100px">
       <span>LOU</span>
     </div>
   </div>
 
-  <span title="ticket link"><a href="https://seatgeek.com/louisville-city-fc-tickets/united-soccer-league/2024-04-06-2-pm/6286554" rel="noopener nofollow" target="_blank">Next Match: April 6<sup>th</sup> 2024</a></span>
+  <span title="ticket link"><!--<a href="https://seatgeek.com/louisville-city-fc-tickets/united-soccer-league/2024-04-06-2-pm/6286554" rel="noopener nofollow" target="_blank">-->Next Match: October 5<sup>th</sup> 2024<!--</a>--></span>
 
 </section>
